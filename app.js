75133796e2d530f0840826c1ae9c2b8b86fa0170
Debadrita Dey <debadritadey08@gmail.com>
2025-06-19 10:37:38 +0530

    hi

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
-app.set("view engine", ejs)
-
+app.set("view engine", "ejs")
+app.get("/",function(req,res){
+    res.render('index')
+})
 app.use("/owners",ownersRouter)
 app.use("/users",usersRouter)
 app.use("/products",productsRouter)
